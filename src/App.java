8eb921373070d70ddc3162654aa6a909af254410
Heytor Mesquita <heytormesquita@gmail.com>
2022-07-28 22:55:24 -0300

    adiciona chamada a api, extracao e listagem de dados dos filmes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // fazer uma conexão HTTP os top 250 filmes
+        // String url = "https://api.themoviedb.org/3/movie/550?api_key=c2ec229ad97dd28e01702cd1b7695692";
+        String url = "https://api.mocki.io/v2/549a5d8b";
+
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
